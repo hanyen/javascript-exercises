29e80f41f07909a03dbb1cce59d5a54db42530d8
hanyen <hanyen@yahoo.com>
2016-08-26 19:54:44 -0700

    Update and rename dragon-slayer to dragon-slayer.js

--- dragon-slayer.js
+++ dragon-slayer.js
@@ -1,32 +1,22 @@
 //variable to check if we are still slaying
 var slaying = true;
 //variable to check if we hit the dragon
-var youHit = 
-
-Math.floor(Math.random()*2);
+var youHit = Math.floor(Math.random()*2);
 //variable to keep track of how much damange we've dealt the dragon this round
-var 
-
-damageThisRound = Math.floor(Math.random()*5+1);
+var damageThisRound = Math.floor(Math.random()*5+1);
 //variable to keep track of total damage
 var totalDamage = 0;
 
-//while 
-
-slaying is true, keep attacking
+//while slaying is true, keep attacking
 while(slaying)
 {
     if (youHit === 1)
     {
         console.log("You hit the dragon");
-   
-
-     totalDamage += damageThisRound;
+      totalDamage += damageThisRound;
         if (totalDamage >= 4)
         {
-            console.log("You killed the 
-
-dragon");
+            console.log("You killed the dragon");
             slaying=false;
         }
     } else
