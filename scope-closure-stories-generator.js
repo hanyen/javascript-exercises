390b74bee889f68ad120f7d51107217013f1b8a9
Hanyen Widjaja <me@hanyen.com>
2016-12-24 15:01:19 -0800

    Added stopwatch function

--- scope-closure-stories-generator.js
+++ scope-closure-stories-generator.js
@@ -25,3 +25,28 @@ var newSaga = function() {
 var result = newSaga();
 result();
 result();
+var result2 = newSaga();
+
+// ===========================================
+
+// Stopwatch 
+
+var makeStopwatch = function() {
+  var elapsed = 0;
+  var increase = function() {
+    elapsed++;
+  }
+  setInterval(increase, 1000);
+  
+  var stopwatch = function() {
+    return elapsed;
+  }
+
+  return stopwatch;
+}
+
+var stopwatch1 = makeStopwatch();
+var stopwatch2 = makeStopwatch(); // create 2nd stopwatch that tracks different elapsed time than the 1st one.
+
+
+
